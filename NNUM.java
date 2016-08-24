6216acc8936fa4e88e41acae6a63f14a6949a737
p-girija <2013girijapriya@gmail.com>
2016-08-24 16:34:09 +0530

    Add files via upload

---
+++ NNUM.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+
+public class NNUM{
+
+	
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n,s=0;
+		n=in.nextInt();
+		for(int i=0;i<=n;i++)
+			s=s+i;
+	
+	System.out.println(s);
+	}
+
+}
