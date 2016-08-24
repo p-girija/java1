389451f5b7d49c61dc331298a754493500bc21bb
p-girija <2013girijapriya@gmail.com>
2016-08-24 17:44:47 +0530

    Add files via upload

---
+++ workday.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class workDay{
+
+	
+	public static void main(String[] args){
+		Scanner in=new Scanner(System.in);
+		String a;
+		String st="";
+		
+		System.out.println("enter string");
+		a=in.next();
+		if(s.equalsIgnoreCase("monday"))
+		
+			st="true";
+		else if(s.equalsIgnoreCase("tuesday"))
+			st="true";
+		
+		else if(s.equalsIgnoreCase("wednesday"))
+			st="true";
+		
+		else if(s.equalsIgnoreCase("thursday"))
+			st="true";
+		
+		else if(s.equalsIgnoreCase("friday"))
+			st="true";
+		
+		else if(s.equalsIgnoreCase("saturday"))
+			st="true";
+		
+		else
+			st="false";
+		
+		System.out.println(st);
+		
+	
+	}
+	
+		
+	}
